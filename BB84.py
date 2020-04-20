4620b0156f8da02534be17d67890d2e4f7159ebd
FerjaniMY <43826213+FerjaniMY@users.noreply.github.com>
2020-04-21 00:57:41 +0100

    Update BB84.py

--- BB84.py
+++ BB84.py
@@ -52,66 +52,14 @@ def receive_qubits(Receiver):
 
 def sifted_key(A_basis,B_basis,key): 
  correct_basis=[]
- raw_key=[]
+ sifted_key=''
 
  for i in range(len(A_basis)):
   if A_basis[i]==B_basis[i]:
     correct_basis.append(i)
-    sifted_key.append(key[i])
+    sifted_key+=str(key[i])
   else:
     pass 
  return sifted_key,correct_basis
- 
-#def reconciliation(keyA,keyB):
- 
 
-
-
-
-"""
-def error_rate(keyA,keyB):
- s=0 
- for i in range(keyA): 
-  if keyA[i]!=keyB[i]:
-   s+=1
- e=s/len(keyA)
- return e
-
-
-def check_basis(A_basis,B_basis):
- correct_basis=[]
- discarded=[]
- for i,b in enumerate(zip(A_basis,B_basis)):
-   if b[0]==b[1]
-    correct_basis.append(i)
-   else:
-    discarded.append(i)
- return correct_basis,discarded
-def check_errors(self, test_bits, peer_test_bits):
-        number_of_errors = len([i for i in range(len(test_bits)) if test_bits[i] != peer_test_bits[i]])
-        delta = number_of_errors/len(test_bits)
-
-def priv_amp(sender,receiver,k): #key=list
- 
- l=[]
- 
- #x=random.sample(k, 2)#Choose randomly two bit of the key
- i=random.randint(0,len(k)-1)
- j=random.randint(0,len(k)-1) 
- while(len(k)!=0):
-  if i!=j:
-   x=int(k[i])
-   del k[i] #delete
-   y=int(k[j])
-   del k[i]
-   xor=(x+y)%2
-   inf=[i,j,xor]
-   l.append(inf)
-   sender.sendClassical(receiver,inf) #send two positions and their xor
-  
-# return l
-"""
-#def reconciliation
-#def error_correction
-#def detect_Eve
 #def QRNG() Quantum Random number generator   
